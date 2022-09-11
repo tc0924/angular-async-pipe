d5752ce97d0f4aebf32aedeb670149a875b52234
tc0924 <test@hoge.com>
2022-09-11 22:50:34 +0900

    表作成の途中

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "../material.module";
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableTestComponent } from './table-test/table-test.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableTestComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
